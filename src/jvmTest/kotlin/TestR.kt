6d71bdb0f8281fdef28d58009fbd8da264ecba9a
hika00 <hika00@gmail.com>
2023-08-20 17:50:57 +0900

    .

--- src/jvmTest/kotlin/TestR.kt
+++ src/jvmTest/kotlin/TestR.kt
@@ -5,6 +5,7 @@ import kotlin.test.assertEquals
 class TestR {
     @Test
     fun test1(){
+        runCatching {  }
         val r1 = R.ok(5)
         assertEquals(r1(), 5)
         val r2 = R.ok { 3 }
@@ -12,35 +13,56 @@ class TestR {
         val r3 = R.fail<Int>(Throwable("test"))
         assertEquals(r3(), null)
         assertEquals(r3{4}, 4)
-        val r4 = R.failInt(Throwable("test"))
-        assertEquals(r4(), null)
     }
     @Test
     fun test2(){
+        var count = 0
         val r1 = R.ok(5)
         val r2 = r1.map{it*3}.map{it+3}
         assertEquals(r2(), 18)
         assertEquals(r1.map{"a"}(), "a")
         val r3 = R.ok{4}
         val r4 = r3.map {
-            println("map1")
+            count++
             it * 2
         }.map{
-            println("map2")
+            count++
             it + 7
         }
-        println("before lazy")
+        assertEquals(count, 0)
         assertEquals(r4(), 15)
-        println("after lazy")
+        assertEquals(count, 2)
         assertEquals(r3.map{"a"}(), "a")
         val r5 = r3.map<Int>{
-            println("m--")
             throw Throwable("1")
         }
         assertEquals(r5{ 10 }, 10)
-        println("a")
         assertEquals(r5(), null)
         val r6 = R.ok(3)
         assertEquals(r6{7}, 3)
+    }
+    @Test
+    fun test3(){
+        var lazyCount = 0
+        val r1 = R.ok(3)
+        val r2 = r1.mapLazy {
+            lazyCount++
+            5
+        }.map {
+            lazyCount++
+            "a"
+        }
+        assertEquals(lazyCount, 0)
+        assertEquals(r2(), "a")
+        assertEquals(lazyCount, 2)
+    }
+    @Test
+    fun test4(){
+        assertEquals(R.catch {
+            3
+        }(), 3)
+        assertEquals(R.catch<Int>{
+            throw Throwable("aa")
+        }(), null)
     }
 }
