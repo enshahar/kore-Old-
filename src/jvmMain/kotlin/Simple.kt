2eedad37bf686f4c1139ed954a5dd90a4da957b3
hika00 <hika00@gmail.com>
2023-08-20 16:00:33 +0900

    .

---
+++ src/jvmMain/kotlin/Simple.kt
@@ -0,0 +1,7 @@
+import kore.r.R
+fun main() {
+    val a = R.ok(3)
+
+}
+
+fun greet() = "world"
---
+++ src/jvmTest/kotlin/TestR.kt
@@ -0,0 +1,35 @@
+import kore.r.R
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class TestR {
+    @Test
+    fun test1(){
+        val r1 = R.ok(5)
+        assertEquals(r1(), 5)
+        val r2 = R.ok { 3 }
+        assertEquals(r2(), 3)
+        val r3 = R.fail<Int>(Throwable("test"))
+        assertEquals(r3(), null)
+        assertEquals(r3{4}, 4)
+        val r4 = R.failInt(Throwable("test"))
+        assertEquals(r4(), null)
+    }
+    @Test
+    fun test2(){
+        val r1 = R.ok(5)
+        val r2 = r1.map{it*3}.map{it+3}
+        assertEquals(r2(), 18)
+        val r3 = R.ok{4}
+        val r4 = r3.map {
+            println("map1")
+            it * 2
+        }.map{
+            println("map2")
+            it + 7
+        }
+        println("before lazy")
+        assertEquals(r4(), 15)
+        println("after lazy")
+    }
+}
