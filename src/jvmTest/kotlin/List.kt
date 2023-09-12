44aa3b4ea4b86383d7a74b9fcf89f748a3231d85
hika00 <hika00@gmail.com>
2023-09-12 18:36:57 +0900

    .

--- src/jvmTest/kotlin/List.kt
+++ src/jvmTest/kotlin/List.kt
@@ -1,47 +1,22 @@
 @file:Suppress("NOTHING_TO_INLINE")
 
-import org.junit.Test
-import kotlin.test.assertEquals
-
-class ListTest{
-    @Test
-    fun test1(){
-        val list = List.of(1,2,3)
-        println("list $list")
-        assertEquals(list.head, 1)
-        assertEquals(list.tail.head, 2)
-        assertEquals(list.tail.tail.head, 3)
-        assertEquals(list.setHead(5).head, 5)
-        assertEquals(list.addFirst(4).head, 4)
-        assertEquals(list.addFirst(4).tail.head, 1)
-        assertEquals(list.drop(2).head, 3)
-        assertEquals(list.drop2(2).head, 3)
-        assertEquals(list.append().head, 1)
-        assertEquals(list.append().tail.head, 2)
-        assertEquals(list.append().tail.tail.head, 3)
-        assertEquals(List.of(1).append(List.of(2,3)).head, 1)
-        assertEquals(List.of(1).append(List.of(2,3)).tail.head, 2)
-        assertEquals(List.of(1).append(List.of(2,3)).tail.tail.head, 3)
-        println("list.dropLast() ${list.dropLast()}")
-        println("list.dropLast().dropLast() ${list.dropLast().dropLast()}")
-        println("list.dropLast(1) ${list.dropLast(1)}")
-        println("list.dropLast(2) ${list.dropLast(2)}")
-        assertEquals(list.dropLast().tail.tail, List.Nil)
-        assertEquals(list.dropLast(2).tail, List.Nil)
-    }
-}
-
 sealed class List<out ITEM:Any>{
     data object Nil:List<Nothing>()
     data class Cons<out ITEM:Any>(@PublishedApi internal val _head:ITEM, @PublishedApi internal val _tail:List<ITEM>):List<ITEM>()
     companion object{
-        fun <ITEM:Any> of(vararg items:ITEM):List<ITEM>{
-            return if(items.isEmpty()) Nil else _of(Cons(items.last(), Nil), items.dropLast(1))
-        }
-        private tailrec fun <ITEM:Any> _of(acc: List<ITEM>, items: kotlin.collections.List<ITEM>): List<ITEM> {
-            return if(items.isEmpty()) acc else _of(Cons(items.last(), acc), items.dropLast(1))
-        }
+        private tailrec fun <ITEM:Any> List<ITEM>.of(items: kotlin.collections.List<ITEM>): List<ITEM>
+            = if(items.isEmpty()) this else Cons(items.last(), this).of(items.dropLast(1))
+        fun <ITEM:Any> of2(vararg items:ITEM):List<ITEM> = Nil.of(items.toList())
+        fun <ITEM:Any> of(vararg items:ITEM):List<ITEM> = items.foldRight(empty(), ::Cons)
+        fun <ITEM:Any> empty():List<ITEM> = Nil
     }
+//    val length:Int = when(this){
+//        is Nil -> 0
+//        is Cons ->when(_tail){
+//            is Nil -> 1
+//            is Cons ->_tail.length + 1
+//        }
+//    }
     inline val head:ITEM? get() = when(this){
         is Nil -> null
         is Cons -> _head
@@ -83,17 +58,33 @@ tailrec fun <ITEM:Any> List<ITEM>.dropWhile(block:(ITEM)->Boolean):List<ITEM> =
     = if(this is List.Cons && block(index, _head)) _tail._dropWhileIndexed(index + 1, block) else this
 fun <ITEM:Any> List<ITEM>.dropWhileIndexed(block:(Int, ITEM)->Boolean):List<ITEM> = _dropWhileIndexed(0, block)
 inline fun <ITEM:Any> List<ITEM>.drop2(n:Int):List<ITEM> = _dropWhileIndexed(0){index, _->index < n}
-
-
 val <ITEM:Any> List<ITEM>.clone:List<ITEM> get() = append()
-fun <ITEM:Any> List<ITEM>.append(list:List<ITEM>? = null):List<ITEM> = when(this){
-    is List.Nil -> list ?: List.Nil
+fun <ITEM:Any> List<ITEM>.append(list:List<ITEM> = List.empty()):List<ITEM> = when(this){
+    is List.Nil -> list
     is List.Cons -> List.Cons(_head, _tail.append(list))
 }
 fun <ITEM:Any> List<ITEM>.dropLast():List<ITEM> = when(this){
-    is List.Nil -> List.Nil
+    is List.Nil -> this
     is List.Cons -> if(_tail is List.Nil) List.Nil else List.Cons(_head, _tail.dropLast())
 }
-@PublishedApi internal tailrec fun <ITEM:Any> _dropLast(acc:List<ITEM>, n:Int):List<ITEM>
-    = if(n == 0) acc else _dropLast(acc.dropLast(), n - 1)
-fun <ITEM:Any> List<ITEM>.dropLast(n:Int):List<ITEM> = _dropLast(this, n)
+tailrec fun <ITEM:Any> List<ITEM>.dropLast(n:Int):List<ITEM>
+    = if(n == 0) this else dropLast().dropLast(n - 1)
+tailrec fun <ITEM:Any> List<ITEM>.dropLastWhile(block:(ITEM)->Boolean):List<ITEM>
+    = if(this is List.Cons && block(_head)) dropLast().dropLastWhile(block) else this
+@PublishedApi internal tailrec fun <ITEM:Any> List<ITEM>._dropLastWhileIndexed(index:Int, block:(Int, ITEM)->Boolean):List<ITEM>
+    = if(this is List.Cons && block(index, _head)) dropLast()._dropLastWhileIndexed(index + 1, block) else this
+fun <ITEM:Any> List<ITEM>.dropLastWhileIndexed(block:(Int, ITEM)->Boolean):List<ITEM> = _dropLastWhileIndexed(0, block)
+tailrec fun <ITEM:Any, ACC:Any> List<ITEM>.fold(acc:ACC, block:(ACC, ITEM)->ACC):ACC = when(this){
+    is List.Nil -> acc
+    is List.Cons -> _tail.fold(block(acc, _head), block)
+}
+fun <ITEM:Any, ACC:Any> List<ITEM>.foldRight(acc:ACC, block:(ITEM, ACC)->ACC):ACC = when(this){
+    is List.Nil -> acc
+    is List.Cons -> block(_head, _tail.foldRight(acc, block))
+}
+val <ITEM:Any> List<ITEM>.clone2:List<ITEM> get() = append2()
+fun <ITEM:Any> List<ITEM>.append2(list:List<ITEM> = List.empty()):List<ITEM> = foldRight(list){it, acc->List.Cons(it, acc)}
+inline val <ITEM:Any> List<ITEM>.size:Int get() = fold(0){acc, _->acc + 1}
+fun <ITEM:Any> List<ITEM>.dropLast2(n:Int = 1):List<ITEM> = foldRight(List.empty()){ it, acc->
+    if(n == 0) List.Cons(it, acc) else acc
+}
---
+++ src/jvmTest/kotlin/ListTest.kt
@@ -0,0 +1,39 @@
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class ListTest{
+    @Test
+    fun test1(){
+        val list = List.of(1, 2, 3)
+        println("list $list")
+        assertEquals(list.head, 1)
+        assertEquals(list.tail.head, 2)
+        assertEquals(list.tail.tail.head, 3)
+        assertEquals(list.setHead(5).head, 5)
+        assertEquals(list.addFirst(4).head, 4)
+        assertEquals(list.addFirst(4).tail.head, 1)
+        assertEquals(list.drop(2).head, 3)
+        assertEquals(list.drop2(2).head, 3)
+        assertEquals(list.dropWhile { it < 2 }.head, 2)
+        assertEquals(list.dropWhileIndexed { index, it -> index < 1 }.head, 2)
+        assertEquals(list.append().head, 1)
+        assertEquals(list.append().tail.head, 2)
+        assertEquals(list.append().tail.tail.head, 3)
+        assertEquals(List.of(1).append(List.of(2, 3)).head, 1)
+        assertEquals(List.of(1).append(List.of(2, 3)).tail.head, 2)
+        assertEquals(List.of(1).append(List.of(2, 3)).tail.tail.head, 3)
+        assertEquals(list.dropLast().tail.tail, List.Nil)
+        assertEquals(list.dropLast(2).tail, List.Nil)
+        assertEquals(list.dropLastWhile { it > 2 }.tail.head, 2)
+        assertEquals(list.dropLastWhileIndexed { index, it -> index > 1 }.tail.head, 2)
+        assertEquals(list.fold(3){acc, it->acc + it}, 9)
+        assertEquals(list.foldRight(3){acc, it->acc + it}, 9)
+        assertEquals(list.append2().head, 1)
+        assertEquals(list.append2().tail.head, 2)
+        assertEquals(list.append2().tail.tail.head, 3)
+        assertEquals(list.dropLast2(2).tail, List.Nil)
+        println("list.dropLast2(2) ${list.dropLast2(1)}")
+        assertEquals(list.dropLast2(1).tail.head, 2)
+    }
+
+}
