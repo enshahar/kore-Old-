1b52470203c9bcf50c23612047b31b32f44f82d8
hika <hika00@gmail.com>
2023-09-15 01:42:53 +0900

    .

--- src/jvmTest/kotlin/Tree.kt
+++ src/jvmTest/kotlin/Tree.kt
@@ -1,8 +1,7 @@
 sealed class Tree<ITEM:Any> {
     companion object{
-        fun <ITEM:Any> of(left:Tree<ITEM>, right:Tree<ITEM>):Tree<ITEM> = Branch(left, right)
-        fun <ITEM:Any> of(left:ITEM, right:ITEM):Tree<ITEM> = Branch(Leaf(left), Leaf(right))
-        fun <ITEM:Any> of(value:ITEM):Tree<ITEM> = Leaf(value)
+        operator fun <ITEM:Any> invoke(left:Tree<ITEM>, right:Tree<ITEM>):Tree<ITEM> = Branch(left, right)
+        operator fun <ITEM:Any> invoke(value:ITEM):Tree<ITEM> = Leaf(value)
     }
     data class Leaf<ITEM:Any>(val item:ITEM):Tree<ITEM>()
     data class Branch<ITEM:Any>(val left:Tree<ITEM>, val right:Tree<ITEM>):Tree<ITEM>()
@@ -15,22 +14,36 @@ fun <ITEM:Any> Tree<ITEM>.setRight(tree: Tree<ITEM>):Tree<ITEM> = when(this){
     is Tree.Leaf -> Tree.Branch(this, tree)
     is Tree.Branch -> Tree.Branch(left, tree)
 }
-fun <ITEM:Any> Tree<ITEM>.size2():Int = when(this){
+val <ITEM:Any> Tree<ITEM>.size:Int get() = when(this){
     is Tree.Leaf -> 1
-    is Tree.Branch -> left.size2() + right.size2()
+    is Tree.Branch -> 1 + left.size + right.size
 }
-inline fun Tree<Int>.size():Int = fold(0){acc, it-> acc + 1}
-fun <ITEM:Any, ACC:Any> Tree<ITEM>.fold(base:ACC, block:(ACC, ITEM)->ACC):ACC = when(this){
-    is Tree.Leaf -> block(base, item)
-    is Tree.Branch -> right.fold(left.fold(base, block), block)
-}
-inline fun Tree<Int>.max():Int = fold(Int.MIN_VALUE){acc, it-> if(acc < it) it else acc}
-
-fun <ITEM:Any> Tree<ITEM>.depth(acc:Int = 0):Int = when(this){
-    is Tree.Leaf -> acc + 1
+val <ITEM> Tree<ITEM>.max:ITEM where ITEM:Comparable<ITEM>, ITEM:Number get() = when(this){
+    is Tree.Leaf -> item
     is Tree.Branch ->{
-        val lDepth = left.depth(acc + 1)
-        val rDepth = right.depth(acc + 1)
-        if(lDepth > rDepth) lDepth else rDepth
+        val lMax = left.max
+        val rMax = right.max
+        if(lMax > rMax) lMax else rMax
     }
 }
+val <ITEM:Any> Tree<ITEM>.depth:Int get() = when(this){
+    is Tree.Leaf -> 1
+    is Tree.Branch ->{
+        val lDepth = left.depth
+        val rDepth = right.depth
+        1 + if(lDepth > rDepth) lDepth else rDepth
+    }
+}
+fun <ITEM:Any, OTHER:Any> Tree<ITEM>.map(block:(ITEM)->OTHER):Tree<OTHER> = when(this){
+    is Tree.Leaf -> Tree.Leaf(block(item))
+    is Tree.Branch -> Tree.Branch(left.map(block), right.map(block))
+}
+fun <ITEM:Any, OTHER:Any> Tree<ITEM>.fold(leafBlock:(ITEM)->OTHER, branchBlock:(l:OTHER, r:OTHER)->OTHER):OTHER = when(this){
+    is Tree.Leaf -> leafBlock(item)
+    is Tree.Branch -> branchBlock(left.fold(leafBlock, branchBlock), right.fold(leafBlock, branchBlock))
+}
+inline val <ITEM:Any> Tree<ITEM>.sizeF:Int get() = fold({1}){l, r->1 + l + r}
+inline val <ITEM> Tree<ITEM>.maxF:ITEM where ITEM:Comparable<ITEM>, ITEM:Number get() = fold({it}){l, r->if(l > r) l else r}
+inline val <ITEM:Any> Tree<ITEM>.depthF:Int get() = fold({1}){l, r->1 + if(l > r) l else r}
+fun <ITEM:Any, OTHER:Any> Tree<ITEM>.mapF(block:(ITEM)->OTHER):Tree<OTHER> = fold({Tree(block(it))}){l, r->Tree(l, r)}
+operator fun <ITEM:Any> Tree<ITEM>.contains(item:ITEM):Boolean = fold({it == item}){l, r-> l || r}
