20b09a5f1df1c4ab1d31ab81a10440fc78b5f8fb
hika00 <hika00@gmail.com>
2023-09-23 22:22:25 +0900

    .

--- src/jvmTest/kotlin/StreamTest.kt
+++ src/jvmTest/kotlin/StreamTest.kt
@@ -9,6 +9,10 @@ class StreamTest {
         assertEquals(Stream(1,2,3,4).take(2).toList().toString(), "Cons(_head=1, _tail=Cons(_head=2, _tail=Nil))")
         assertEquals(Stream(1,2,3,4).drop(2).toList().toString(), "Cons(_head=3, _tail=Cons(_head=4, _tail=Nil))")
         assertEquals(Stream(1,2,3,4).takeWhile{it<3}.toList().toString(), "Cons(_head=1, _tail=Cons(_head=2, _tail=Nil))")
-
+        assertEquals(Stream(1,2,3,4).any{it == 2}, true)
+        assertEquals(Stream(1,2,3,4).any{it == 5}, false)
+        assertEquals(Stream(1,2,3,4).map{"-$it"}.toList().toString(), "Cons(_head=-1, _tail=Cons(_head=-2, _tail=Cons(_head=-3, _tail=Cons(_head=-4, _tail=Nil))))")
+        assertEquals(Stream(1,2,3,4).filter{it<3}.toList().toString(), "Cons(_head=1, _tail=Cons(_head=2, _tail=Nil))")
+        assertEquals(Stream(1,2).append{Stream(3,4)}.toList().toString(), "Cons(_head=1, _tail=Cons(_head=2, _tail=Cons(_head=3, _tail=Cons(_head=4, _tail=Nil))))")
     }
 }
---
+++ src/jvmTest/kotlin/Value.kt
@@ -0,0 +1,14 @@
+sealed class Value<T>{
+    companion object{
+        operator fun <T> invoke(value:T): Value<T> = Evaluated(value)
+        operator fun <T> invoke(block:()->T): Value<T> = Unevaluated(block)
+    }
+    data class Evaluated<T> internal constructor(private val value:T): Value<T>(){
+        override fun invoke(): T = value
+    }
+    data class Unevaluated<T> internal constructor(private val block:()->T): Value<T>(){
+        private var memo:T? = null
+        override fun invoke(): T  = memo ?: block().also { memo = it }
+    }
+    abstract operator fun invoke():T
+}
