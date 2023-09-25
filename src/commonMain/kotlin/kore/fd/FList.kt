4f9c8c592358c9e91fe489273ad7ef1ba8fcf795
hika <hika00@gmail.com>
2023-09-25 14:05:26 +0900

    .

--- src/commonMain/kotlin/kore/fd/FList.kt
+++ src/commonMain/kotlin/kore/fd/FList.kt
@@ -1,4 +1,4 @@
-@file:Suppress("NOTHING_TO_INLINE")
+@file:Suppress("NOTHING_TO_INLINE", "FunctionName")
 
 package kore.fd
 
@@ -34,12 +34,12 @@ inline fun <ITEM:Any> FList<ITEM>.addFirst(item:ITEM):FList<ITEM> = when(this){
     is Cons -> Cons(item, this)
     is Nil -> FList()
 }
-/**-----------------------------------------------------------------*/
-tailrec fun <ITEM:Any, ACC:Any> FList<ITEM>.fold(base:ACC, block:(ACC, ITEM)->ACC):ACC = when(this){
-    is Cons -> tail.fold(block(base, head), block)
-    is Nil -> base
+//** base-----------------------------------------------------------------*/
+tailrec fun <ITEM:Any, ACC:Any> FList<ITEM>.fold(acc:ACC, block:(ACC, ITEM)->ACC):ACC = when(this){
+    is Cons -> tail.fold(block(acc, head), block)
+    is Nil -> acc
 }
-inline fun <ITEM:Any> FList<ITEM>.reverse(): FList<ITEM> = fold(FList.invoke()){ acc, it->Cons(it, acc)}
+inline fun <ITEM:Any> FList<ITEM>.reverse(): FList<ITEM> = fold(FList()){acc, it->Cons(it, acc)}
 inline fun <ITEM:Any, ACC:Any> FList<ITEM>.foldRight(base:ACC, crossinline block:(ITEM, ACC)->ACC):ACC = reverse().fold(base){ acc, it->block(it, acc)}
 inline fun <ITEM:Any, OTHER:Any> FList<ITEM>.map(crossinline block:(ITEM)->OTHER): FList<OTHER> = foldRight(FList()){ it, acc->Cons(block(it), acc)}
 @PublishedApi internal tailrec fun <ITEM:Any, OTHER:Any> FList<ITEM>._flatMap(acc:FList<OTHER>, block:(ITEM)-> FList<OTHER>):FList<OTHER> = when(this){
@@ -50,27 +50,29 @@ inline fun <ITEM:Any, OTHER:Any> FList<ITEM>.map(crossinline block:(ITEM)->OTHER
     is Nil -> acc
 }
 inline fun <ITEM:Any, OTHER:Any> FList<ITEM>.flatMap(noinline block:(ITEM)-> FList<OTHER>): FList<OTHER> = _flatMap(FList(), block)
-///**-----------------------------------------------------------------*/
+fun <ITEM:Any> FList<FList<ITEM>>.flatten(): FList<ITEM> = reverse().fold<FList<ITEM>, FList<ITEM>>(FList()){acc, it->
+    it.reverse().fold(acc){acc2, it2->Cons(it2, acc2)}
+}
+//** append-----------------------------------------------------------------*/
 inline fun <ITEM:Any> FList<ITEM>.append(list: FList<ITEM> = FList.invoke()): FList<ITEM> = foldRight(list, ::Cons)
 inline fun <ITEM:Any> FList<ITEM>.copy():FList<ITEM> = append()
 inline operator fun <ITEM:Any> FList<ITEM>.plus(list:FList<ITEM>):FList<ITEM> = append(list)
-/**-----------------------------------------------------------------*/
+//** drop----------------------------------------------------------*/
+@PublishedApi internal tailrec fun <ITEM:Any> FList<ITEM>._dropWhileIndexed(index:Int, block:(Int, ITEM)->Boolean): FList<ITEM>
+= if(this is Cons && block(index, head)) tail._dropWhileIndexed(index + 1, block) else this
+inline fun <ITEM:Any> FList<ITEM>.dropWhileIndexed(noinline block:(Int, ITEM)->Boolean):FList<ITEM> = _dropWhileIndexed(0, block)
 tailrec fun <ITEM:Any> FList<ITEM>.drop(n:Int = 1): FList<ITEM> = if(n > 0 && this is Cons) tail.drop(n - 1) else this
 tailrec fun <ITEM:Any> FList<ITEM>.dropWhile(block:(ITEM)->Boolean): FList<ITEM> = if(this is Cons && block(head)) tail.dropWhile(block) else this
-@PublishedApi internal tailrec fun <ITEM:Any> FList<ITEM>._dropWhileIndexed(index:Int, block:(Int, ITEM)->Boolean): FList<ITEM> = if(this is Cons && block(index, head)) tail._dropWhileIndexed(index + 1, block) else this
-fun <ITEM:Any> FList<ITEM>.dropWhileIndexed(block:(Int, ITEM)->Boolean):FList<ITEM> = _dropWhileIndexed(0, block)
+//** dropLast----------------------------------------------------------*/
 
-@PublishedApi internal tailrec fun <ITEM:Any> FList<ITEM>._dropLast(n:Int):FList<ITEM> = when(this){
+@PublishedApi internal tailrec fun <ITEM:Any> FList<ITEM>._dropLastWhileIndexed(index:Int, block:(Int, ITEM)->Boolean):FList<ITEM>
+= when(this){
+    is Cons -> if(!block(index, head)) reverse() else tail._dropLastWhileIndexed(index + 1, block)
     is Nil -> reverse()
-    is Cons -> if(n == 0) reverse() else tail._dropLast(n - 1)
 }
-fun <ITEM:Any> FList<ITEM>.dropLast(n:Int = 0):FList<ITEM> = reverse()._dropLast(n)
-        //= if(n == 0) this else dropLast().dropLast(n - 1)
-//tailrec fun <ITEM:Any> FList<ITEM>.dropLastWhile(block:(ITEM)->Boolean): FList<ITEM>
-//        = if(this is Cons && block(_head)) dropLast().dropLastWhile(block) else this
-//@PublishedApi internal tailrec fun <ITEM:Any> FList<ITEM>._dropLastWhileIndexed(index:Int, block:(Int, ITEM)->Boolean): FList<ITEM>
-//        = if(this is Cons && block(index, _head)) dropLast()._dropLastWhileIndexed(index + 1, block) else this
-//fun <ITEM:Any> FList<ITEM>.dropLastWhileIndexed(block:(Int, ITEM)->Boolean): FList<ITEM> = _dropLastWhileIndexed(0, block)
+inline fun <ITEM:Any> FList<ITEM>.dropLastWhileIndexed(noinline block:(Int, ITEM)->Boolean):FList<ITEM> = reverse()._dropLastWhileIndexed(0, block)
+inline fun <ITEM:Any> FList<ITEM>.dropLastWhile(noinline block:(ITEM)->Boolean):FList<ITEM> = reverse()._dropLastWhileIndexed(0){_,it->block(it)}
+fun <ITEM:Any> FList<ITEM>.dropLast(n:Int = 1):FList<ITEM> = reverse()._dropLastWhileIndexed(0){index,_->index<n}
 
 ///**-----------------------------------------------------------------*/
 //fun <ITEM:Any> FList<ITEM>.filter(block:(ITEM)->Boolean): FList<ITEM> = foldRight(FList()){it, acc->
@@ -115,12 +117,7 @@ fun <ITEM:Any> FList<ITEM>.dropLast(n:Int = 0):FList<ITEM> = reverse()._dropLast
 //
 //
 //
-//fun <ITEM:Any> FList<FList<ITEM>>.flatten(): FList<ITEM> = when(this){
-//    is Nil -> this
-//    is Cons -> drop(1).fold(_head){ acc, it->
-//        acc.append(it)
-//    }
-//}
+
 //@PublishedApi internal tailrec fun <ITEM:Any, OTHER:Any, RESULT:Any> FList<ITEM>._zipWithFold(other: FList<OTHER>, base: FList<RESULT>, block:(ITEM, OTHER)->RESULT): FList<RESULT> = when(this){
 //    is Nil -> base
 //    is Cons -> when(other){
