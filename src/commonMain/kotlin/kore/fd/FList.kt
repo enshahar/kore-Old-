0d1999d91c2cf09ac4512c8946978872af7c743f
hika00 <hika00@gmail.com>
2023-09-25 18:26:41 +0900

    .

--- src/commonMain/kotlin/kore/fd/FList.kt
+++ src/commonMain/kotlin/kore/fd/FList.kt
@@ -20,39 +20,37 @@ sealed class FList<out ITEM:Any>{
         }
         is Nil -> 0
     }
-    inline fun getHeadOrNull():ITEM? = when(this){
-        is Cons -> head
-        is Nil -> null
-    }
-
 }
+inline fun <ITEM:Any> FList<ITEM>.toList():List<ITEM> = fold(listOf()){acc, it->acc + it}
 inline fun <ITEM:Any> FList<ITEM>.setHead(item:ITEM):FList<ITEM> = when(this){
     is Cons -> Cons(item, tail)
-    is Nil -> FList()
+    is Nil -> this
 }
 inline fun <ITEM:Any> FList<ITEM>.addFirst(item:ITEM):FList<ITEM> = when(this){
     is Cons -> Cons(item, this)
-    is Nil -> FList()
+    is Nil -> this
 }
 //** base-----------------------------------------------------------------*/
 tailrec fun <ITEM:Any, ACC:Any> FList<ITEM>.fold(acc:ACC, block:(ACC, ITEM)->ACC):ACC = when(this){
     is Cons -> tail.fold(block(acc, head), block)
     is Nil -> acc
 }
-inline fun <ITEM:Any> FList<ITEM>.reverse(): FList<ITEM> = fold(FList()){acc, it->Cons(it, acc)}
-inline fun <ITEM:Any, ACC:Any> FList<ITEM>.foldRight(base:ACC, crossinline block:(ITEM, ACC)->ACC):ACC = reverse().fold(base){ acc, it->block(it, acc)}
-inline fun <ITEM:Any, OTHER:Any> FList<ITEM>.map(crossinline block:(ITEM)->OTHER): FList<OTHER> = foldRight(FList()){ it, acc->Cons(block(it), acc)}
-@PublishedApi internal tailrec fun <ITEM:Any, OTHER:Any> FList<ITEM>._flatMap(acc:FList<OTHER>, block:(ITEM)-> FList<OTHER>):FList<OTHER> = when(this){
-    is Cons -> when(val v = block(head)){
-        is Cons ->tail._flatMap(acc + v, block)
-        is Nil ->tail._flatMap(acc, block)
-    }
+@PublishedApi internal tailrec fun <ITEM:Any, ACC:Any> FList<ITEM>._foldIndexed(index:Int, acc:ACC, block:(Int, ACC, ITEM)->ACC):ACC = when(this){
+    is Cons -> tail._foldIndexed(index + 1, block(index, acc, head), block)
     is Nil -> acc
 }
-inline fun <ITEM:Any, OTHER:Any> FList<ITEM>.flatMap(noinline block:(ITEM)-> FList<OTHER>): FList<OTHER> = _flatMap(FList(), block)
-fun <ITEM:Any> FList<FList<ITEM>>.flatten(): FList<ITEM> = reverse().fold<FList<ITEM>, FList<ITEM>>(FList()){acc, it->
-    it.reverse().fold(acc){acc2, it2->Cons(it2, acc2)}
+inline fun <ITEM:Any, ACC:Any> FList<ITEM>.foldIndexed(base:ACC, noinline block:(Int, ACC, ITEM)->ACC):ACC = _foldIndexed(0, base, block)
+inline fun <ITEM:Any> FList<ITEM>.reverse(): FList<ITEM> = fold(FList()){acc, it->Cons(it, acc)}
+inline fun <ITEM:Any, ACC:Any> FList<ITEM>.foldRight(base:ACC, crossinline block:(ITEM, ACC)->ACC):ACC = reverse().fold(base){ acc, it->block(it, acc)}
+fun <ITEM:Any, ACC:Any> FList<ITEM>.foldRightIndexed(base:ACC, block:(Int, ITEM, ACC)->ACC):ACC = reverse()._foldIndexed(0, base){index, acc, it->block(index, it, acc)}
+inline fun <ITEM:Any, OTHER:Any> FList<ITEM>.map(crossinline block:(ITEM)->OTHER): FList<OTHER> = foldRight(FList()){ it, acc->Cons(block(it), acc)}
+inline fun <ITEM:Any, OTHER:Any> FList<ITEM>.flatMap(noinline block:(ITEM)-> FList<OTHER>):FList<OTHER> = foldRight(FList()){it, acc->
+    when(val v = block(it)){
+        is Cons ->v.foldRight(acc, ::Cons)
+        is Nil ->acc
+    }
 }
+fun <ITEM:Any> FList<FList<ITEM>>.flatten(): FList<ITEM> = foldRight(FList()){it, acc->it.foldRight(acc, ::Cons)}
 //** append-----------------------------------------------------------------*/
 inline fun <ITEM:Any> FList<ITEM>.append(list: FList<ITEM> = FList.invoke()): FList<ITEM> = foldRight(list, ::Cons)
 inline fun <ITEM:Any> FList<ITEM>.copy():FList<ITEM> = append()
@@ -64,7 +62,6 @@ inline fun <ITEM:Any> FList<ITEM>.dropWhileIndexed(noinline block:(Int, ITEM)->B
 tailrec fun <ITEM:Any> FList<ITEM>.drop(n:Int = 1): FList<ITEM> = if(n > 0 && this is Cons) tail.drop(n - 1) else this
 tailrec fun <ITEM:Any> FList<ITEM>.dropWhile(block:(ITEM)->Boolean): FList<ITEM> = if(this is Cons && block(head)) tail.dropWhile(block) else this
 //** dropLast----------------------------------------------------------*/
-
 @PublishedApi internal tailrec fun <ITEM:Any> FList<ITEM>._dropLastWhileIndexed(index:Int, block:(Int, ITEM)->Boolean):FList<ITEM>
 = when(this){
     is Cons -> if(!block(index, head)) reverse() else tail._dropLastWhileIndexed(index + 1, block)
@@ -72,85 +69,52 @@ tailrec fun <ITEM:Any> FList<ITEM>.dropWhile(block:(ITEM)->Boolean): FList<ITEM>
 }
 inline fun <ITEM:Any> FList<ITEM>.dropLastWhileIndexed(noinline block:(Int, ITEM)->Boolean):FList<ITEM> = reverse()._dropLastWhileIndexed(0, block)
 inline fun <ITEM:Any> FList<ITEM>.dropLastWhile(noinline block:(ITEM)->Boolean):FList<ITEM> = reverse()._dropLastWhileIndexed(0){_,it->block(it)}
-fun <ITEM:Any> FList<ITEM>.dropLast(n:Int = 1):FList<ITEM> = reverse()._dropLastWhileIndexed(0){index,_->index<n}
-
-///**-----------------------------------------------------------------*/
-//fun <ITEM:Any> FList<ITEM>.filter(block:(ITEM)->Boolean): FList<ITEM> = foldRight(FList()){it, acc->
-//    if(block(it)) Cons(it, acc) else acc
-//}
-//
-//
-//@PublishedApi internal tailrec fun <ITEM:Any, ACC:Any> FList<ITEM>._foldIndexed(index:Int, base:ACC, block:(Int, ACC, ITEM)->ACC):ACC = when(this){
-//    is Nil -> base
-//    is Cons -> _tail._foldIndexed(index + 1, block(index, base, _head), block)
-//}
-//fun <ITEM:Any, ACC:Any> FList<ITEM>.foldIndexed(base:ACC, block:(Int, ACC, ITEM)->ACC):ACC = _foldIndexed(0, base, block)
-//
-//
-//
-//@PublishedApi internal tailrec fun <ITEM:Any, ACC:Any> FList<ITEM>._foldRightIndexed(index:Int, base:ACC, block:(Int, ITEM, ACC)->ACC):ACC = when(this){
-//    is Nil -> base
-//    is Cons -> _tail._foldRightIndexed(index + 1, block(index, _head, base), block)
-//}
-//fun <ITEM:Any, ACC:Any> FList<ITEM>.foldRightIndexed(base:ACC, block:(Int, ITEM, ACC)->ACC):ACC = reverse()._foldRightIndexed(0, base, block)
-//
-//
-//@PublishedApi internal fun <ITEM:Any, ACC:Any> FList<ITEM>._foldRightIndexed2(index:Int, base:ACC, block:(Int, ITEM, ACC)->ACC):ACC = when(this){
-//    is Nil -> base
-//    is Cons -> block(index, _head, _tail._foldRightIndexed2(index - 1, base, block))
-//}
-//fun <ITEM:Any, ACC:Any> FList<ITEM>.foldRightIndexed2(base:ACC, block:(Int, ITEM, ACC)->ACC):ACC = _foldRightIndexed2(size - 1, base, block)
-//
-//inline val <ITEM:Any> FList<ITEM>.size:Int get() = fold(0){ acc, _->acc + 1}
-//fun <ITEM:Any> FList<ITEM>.dropLast2(n:Int = 1): FList<ITEM> = foldRightIndexed(FList.invoke()){ index, it, acc->
-//    if(index >= n) Cons(it, acc) else FList.invoke()
-//}
-//inline fun FList<Int>.sum():Int = fold(0){ acc, it-> acc + it}
-//inline fun FList<Long>.sum():Long = fold(0L){ acc, it-> acc + it}
-//inline fun FList<Float>.sum():Float = fold(0.0f){ acc, it-> acc + it}
-//inline fun FList<Double>.sum():Double = fold(0.0){ acc, it-> acc + it}
-//
-//inline fun FList<Int>.product():Int = fold(0){ acc, it-> acc * it}
-//inline fun FList<Long>.product():Long = fold(0L){ acc, it-> acc * it}
-//inline fun FList<Float>.product():Float = fold(0.0f){ acc, it-> acc * it}
-//inline fun FList<Double>.product():Double = fold(0.0){ acc, it-> acc * it}
-//
-//
-//
-
-//@PublishedApi internal tailrec fun <ITEM:Any, OTHER:Any, RESULT:Any> FList<ITEM>._zipWithFold(other: FList<OTHER>, base: FList<RESULT>, block:(ITEM, OTHER)->RESULT): FList<RESULT> = when(this){
-//    is Nil -> base
-//    is Cons -> when(other){
-//        is Nil -> base
-//        is Cons -> _tail._zipWithFold(other.tail, Cons(block(_head, other._head), base), block)
-//    }
-//}
-//fun <ITEM:Any, OTHER:Any, RESULT:Any> FList<ITEM>.zipWith(other: FList<OTHER>, block:(ITEM, OTHER)->RESULT): FList<RESULT> {
-//     return if(other.size > size){
-//         reverse()._zipWithFold(other.dropLast(other.size - size).reverse(), FList.invoke(), block)
-//     }else{
-//         other.reverse()._zipWithFold(dropLast(size - other.size).reverse(), FList.invoke(), { a, b->block(b, a)})
-//     }
-//}
-//tailrec fun <ITEM:Any> FList<ITEM>.sliceFrom(item:ITEM): FList<ITEM> = when(this){
-//    is Nil -> Nil
-//    is Cons -> if(_head == item) this else _tail.sliceFrom(item)
-//}
-//tailrec fun <ITEM:Any> FList<ITEM>.startsWith(target: FList<ITEM>):Boolean = when(this) {
-//    is Nil -> true
-//    is Cons ->if(target is Cons && _head == target._head) _tail.startsWith(target._tail) else false
-////    when(target){
-////        is List.Nil -> false
-////        is List.Cons -> if(_head != target._head) false else _tail.isSameItems(target._tail)
-////    }
-//}
-//fun <ITEM:Any> FList<ITEM>.hasSubSequence(sub: FList<ITEM>):Boolean = when(this) {
-//    is Nil -> sub is Nil
-//    is Cons -> when (sub) {
-//        is Nil -> true
-//        is Cons -> when (val subList = sliceFrom(sub._head)) {
-//            is Nil -> false
-//            is Cons -> sub.startsWith(subList)
-//        }
-//    }
-//}
+fun <ITEM:Any> FList<ITEM>.dropLast(n:Int = 1):FList<ITEM> = reverse()._dropLastWhileIndexed(0){index,_->index < n}
+//** utils-----------------------------------------------------------------*/
+fun <ITEM:Any> FList<ITEM>.filter(block:(ITEM)->Boolean): FList<ITEM> = foldRight(FList()){it, acc->if(block(it)) Cons(it, acc) else acc}
+tailrec fun <ITEM:Any> FList<ITEM>.sliceFrom(item:ITEM): FList<ITEM> = if(this is Cons && head != item) tail.sliceFrom(item) else this
+tailrec fun <ITEM:Any> FList<ITEM>.slice(from:Int): FList<ITEM> = if(this is Cons && from > 0) tail.slice(from - 1) else this
+inline fun <ITEM:Any> FList<ITEM>.slice(from:Int, to:Int): FList<ITEM> = slice(from).dropLast(to - from)
+tailrec fun <ITEM:Any> FList<ITEM>.startsWith(target:FList<ITEM>):Boolean = when(this) {
+    is Cons ->{
+        when(target){
+            is Cons -> if(head == target.head) when(target.tail){
+                is Cons-> tail.startsWith(target.tail)
+                is Nil -> true
+            } else false
+            is Nil -> false
+        }
+    }
+    is Nil -> target is Nil
+}
+tailrec operator fun <ITEM:Any> FList<ITEM>.contains(target:ITEM):Boolean = when(this) {
+    is Cons -> if(head == target) true else target in tail
+    is Nil -> false
+}
+tailrec operator fun <ITEM:Any> FList<ITEM>.contains(target: FList<ITEM>):Boolean = when(this) {
+    is Cons -> when (target) {
+        is Cons -> if(startsWith(target)) true else target in tail
+        is Nil -> false
+    }
+    is Nil -> target is Nil
+}
+inline fun <ITEM:Any, OTHER:Any, RESULT:Any> FList<ITEM>.zipWith(other: FList<OTHER>, noinline block:(ITEM, OTHER)->RESULT): FList<RESULT> {
+    if(this is Nil || other is Nil) return FList()
+    val thisSize = size
+    val otherSize = other.size
+    return if(otherSize > thisSize){
+        foldRight(FList<RESULT>() to other.dropLast(otherSize - thisSize).reverse()){it, (acc, target)->
+            when(target){
+                is Cons -> Cons(block(it, target.head), acc) to target.tail
+                is Nil -> acc to target
+            }
+        }.first
+    } else {
+        other.foldRight(FList<RESULT>() to dropLast(thisSize - otherSize).reverse()){it, (acc, target)->
+            when(target){
+                is Cons -> Cons(block(target.head, it), acc) to target.tail
+                is Nil -> acc to target
+            }
+        }.first
+    }
+}
--- src/jvmTest/kotlin/fd/TreeTest.kt
+++ src/jvmTest/kotlin/fd/TreeTest.kt
@@ -6,23 +6,11 @@ import kotlin.test.assertEquals
 class TreeTest {
     @Test
     fun test1(){
-        val tree = Tree(Tree(Tree(1), Tree(2)), Tree(Tree(7), Tree(4)))
-//        assertEquals(tree.size(), 4)
-//        assertEquals(tree.size2(), 4)
-//        assertEquals(tree.size3(), 4)
-//        assertEquals(tree.fold(Int.MIN_VALUE){acc, it-> if(acc < it) it else acc}, 7)
-//        assertEquals(tree.max(), 7)
-//        assertEquals(Tree.of(Tree.of(1,2), Tree.of(3)).depth(), 3)
-
-        val tree2 = Tree(Tree(Tree(1), Tree(Tree(3), Tree(4))), Tree(5))
+        val tree2 = FTree(FTree(FTree(1), FTree(FTree(3), FTree(4))), FTree(5))
         assertEquals(tree2.depth, 4)
-        assertEquals(tree2.depthF, 4)
         assertEquals(tree2.size, 7)
-        assertEquals(tree2.sizeF, 7)
         assertEquals(tree2.max, 5)
-        assertEquals(tree2.maxF, 5)
         assertEquals("${tree2.map{it*2}}", "Branch(left=Branch(left=Leaf(item=2), right=Branch(left=Leaf(item=6), right=Leaf(item=8))), right=Leaf(item=10))")
-        assertEquals("${tree2.mapF{it*2}}", "Branch(left=Branch(left=Leaf(item=2), right=Branch(left=Leaf(item=6), right=Leaf(item=8))), right=Leaf(item=10))")
         assertEquals(1 in tree2, true)
         assertEquals(3 in tree2, true)
         assertEquals(4 in tree2, true)
