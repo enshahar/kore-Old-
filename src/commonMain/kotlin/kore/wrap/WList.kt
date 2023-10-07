75172028bfa30cdc99419bd032ca883c55bd9806
hika <hika00@gmail.com>
2023-10-07 20:20:43 +0900

    .

--- src/commonMain/kotlin/kore/wrap/WList.kt
+++ src/commonMain/kotlin/kore/wrap/WList.kt
@@ -44,27 +44,20 @@ inline fun <ITEM:Any> WList<ITEM>.addFirstW(item:Wrap<ITEM>): WList<ITEM> = when
 //** base-----------------------------------------------------------------*/
 tailrec fun <ITEM:Any, ACC:Any> WList<ITEM>.foldW(acc:Wrap<ACC>, block:(Wrap<ACC>, ITEM)->Wrap<ACC>):Wrap<ACC>{
     return when(this){
-        is Cons-> {
-            val tailV = tail.getOrFailEffect{return W(it)}
-            tailV.foldW(block(acc, head).failEffect{return W(it)}, block)
-        }
+        is Cons-> tail.getOrFailEffect{return W(it)}.foldW(block(acc, head).failEffect{return W(it)}, block)
         is Nil-> acc
     }
 }
 tailrec fun <ITEM:Any, ACC:Any> WList<ITEM>.fold(acc:ACC, block:(ACC, ITEM)->ACC):ACC{
     return when(this){
-        is Cons-> {
-            val tailV = tail.getOrFailEffect{throw it}
-            tailV.fold(block(acc, head), block)
-        }
+        is Cons-> tail.getOrFailEffect{throw it}.fold(block(acc, head), block)
         is Nil-> acc
     }
 }
 @PublishedApi internal tailrec fun <ITEM:Any, ACC:Any> WList<ITEM>._foldIndexed(index:Int, acc:ACC, block:(Int, ACC, ITEM)->ACC):ACC
 = when(this){
     is Cons ->{
-        val tailV = tail.getOrFailEffect{throw it}
-        tailV._foldIndexed(index + 1, block(index, acc, head), block)
+        tail.getOrFailEffect{throw it}._foldIndexed(index + 1, block(index, acc, head), block)
     }
     is Nil -> acc
 }
@@ -72,10 +65,7 @@ inline fun <ITEM:Any, ACC:Any> WList<ITEM>.foldIndexed(base:ACC, noinline block:
 = _foldIndexed(0, base, block)
 @PublishedApi internal tailrec fun <ITEM:Any, ACC:Any> WList<ITEM>._foldIndexedW(index:Int, acc:Wrap<ACC>, block:(Int, Wrap<ACC>, ITEM)->Wrap<ACC>):Wrap<ACC>
 = when(this){
-    is Cons ->{
-        val tailV = tail.getOrFailEffect{return W(it)}
-        tailV._foldIndexedW(index + 1, block(index, acc, head).failEffect{return W(it)}, block)
-    }
+    is Cons ->tail.getOrFailEffect{return W(it)}._foldIndexedW(index + 1, block(index, acc, head).failEffect{return W(it)}, block)
     is Nil -> acc
 }
 inline fun <ITEM:Any, ACC:Any> WList<ITEM>.foldIndexedW(base:Wrap<ACC>, noinline block:(Int, Wrap<ACC>, ITEM)->Wrap<ACC>):Wrap<ACC>
@@ -102,56 +92,62 @@ inline fun <ITEM:Any, OTHER:Any> WList<ITEM>.flatMap(noinline block:(ITEM)->WLis
         }
     }
 }
-//fun <ITEM:Any> WList<WList<ITEM>>.flatten(): Wrap<WList<ITEM>>
-//= foldRight(W{WList()}){ it, acc->it().foldRight(acc){ it2, acc2->W{Cons(it2, acc2)}}
+fun <ITEM:Any> WList<WList<ITEM>>.flatten(): WList<ITEM>
+= foldRight(WList()){it, acc->it.foldRight(acc){ it2, acc2->Cons(it2, W(acc2))}}
 ////** append-----------------------------------------------------------------*/
-//fun <ITEM:Any> WList<ITEM>.append(list: Wrap<WList<ITEM>> = W{WList()}): WList<ITEM> = foldRight(list){ it, acc->W{Cons(it, acc)}}()
-//fun <ITEM:Any> WList<ITEM>.copy():WList<ITEM> = append()
-//operator fun <ITEM:Any> WList<ITEM>.plus(list:WList<ITEM>):WList<ITEM> = append(W{list})
+fun <ITEM:Any> WList<ITEM>.appendW(list:Wrap<WList<ITEM>> = W{WList()}): Wrap<WList<ITEM>> = foldRightW(list){it, acc->W{Cons(it, acc)}}
+fun <ITEM:Any> WList<ITEM>.append(list:WList<ITEM> = WList()): WList<ITEM> = foldRight(list){it, acc->Cons(it, W{acc})}
+fun <ITEM:Any> WList<ITEM>.copy():WList<ITEM> = append()
+operator fun <ITEM:Any> WList<ITEM>.plus(list:WList<ITEM>):WList<ITEM> = append(list)
+fun <ITEM:Any> WList<ITEM>.copyW():Wrap<WList<ITEM>> = appendW()
+operator fun <ITEM:Any> WList<ITEM>.plus(list:Wrap<WList<ITEM>>):Wrap<WList<ITEM>> = appendW(list)
 ////** drop----------------------------------------------------------*/
-//tailrec fun <ITEM:Any> WList<ITEM>._dropWhileIndexed(index:Int, block:(Int, Wrap<ITEM>)->Boolean): WList<ITEM>
-//= if(this is Cons && block(index, head)) tail()._dropWhileIndexed(index + 1, block) else this
-//fun <ITEM:Any> WList<ITEM>.dropWhileIndexed(block:(Int, Wrap<ITEM>)->Boolean):WList<ITEM> = _dropWhileIndexed(0, block)
-//tailrec fun <ITEM:Any> WList<ITEM>.drop(n:Int = 1): WList<ITEM> = if(n > 0 && this is Cons) tail().drop(n - 1) else this
-//tailrec fun <ITEM:Any> WList<ITEM>.dropWhile(block:(Wrap<ITEM>)->Boolean): WList<ITEM> = if(this is Cons && block(head)) tail().dropWhile(block) else this
+tailrec fun <ITEM:Any> WList<ITEM>._dropWhileIndexed(index:Int, block:(Int, ITEM)->Boolean): WList<ITEM>
+= if(this is Cons && block(index, head)) tail.getOrFailEffect{return WList()}._dropWhileIndexed(index + 1, block) else this
+inline fun <ITEM:Any> WList<ITEM>.dropWhileIndexed(noinline block:(Int, ITEM)->Boolean):WList<ITEM> = _dropWhileIndexed(0, block)
+tailrec fun <ITEM:Any> WList<ITEM>.drop(n:Int = 1): WList<ITEM>
+= if(n > 0 && this is Cons) tail.getOrFailEffect{return WList()}.drop(n - 1) else this
+tailrec fun <ITEM:Any> WList<ITEM>.dropWhile(block:(ITEM)->Boolean): WList<ITEM>
+= if(this is Cons && block(head)) tail.getOrFailEffect{return WList()}.dropWhile(block) else this
 ////** dropLast----------------------------------------------------------*/
-//tailrec fun <ITEM:Any> WList<ITEM>._dropLastWhileIndexed(index:Int, block:(Int, Wrap<ITEM>)->Boolean):Wrap<WList<ITEM>>
-//= when(this){
-//    is Cons -> if(!block(index, head)) reverse() else tail()._dropLastWhileIndexed(index + 1, block)
-//    is Nil -> reverse()
-//}
-//fun <ITEM:Any> WList<ITEM>.dropLastWhileIndexed(block:(Int, Wrap<ITEM>)->Boolean):WList<ITEM> = reverse()()._dropLastWhileIndexed(0, block)()
-//fun <ITEM:Any> WList<ITEM>.dropLastWhile(block:(Wrap<ITEM>)->Boolean):WList<ITEM> = reverse()()._dropLastWhileIndexed(0){ _, it->block(it)}()
-//fun <ITEM:Any> WList<ITEM>.dropLast(n:Int = 1):WList<ITEM> = reverse()()._dropLastWhileIndexed(0){ index, _->index < n}()
+@PublishedApi internal tailrec fun <ITEM:Any> WList<ITEM>._dropLastWhileIndexed(index:Int, block:(Int, ITEM)->Boolean):WList<ITEM>
+= when(this){
+    is Cons -> if(!block(index, head)) reverse() else tail.getOrFailEffect{return WList()}._dropLastWhileIndexed(index + 1, block)
+    is Nil -> reverse()
+}
+fun <ITEM:Any> WList<ITEM>.dropLastWhileIndexed(block:(Int, ITEM)->Boolean):WList<ITEM> = reverse()._dropLastWhileIndexed(0, block)
+fun <ITEM:Any> WList<ITEM>.dropLastWhile(block:(ITEM)->Boolean):WList<ITEM> = reverse()._dropLastWhileIndexed(0){ _, it->block(it)}
+fun <ITEM:Any> WList<ITEM>.dropLast(n:Int = 1):WList<ITEM> = reverse()._dropLastWhileIndexed(0){ index, _->index < n}
 ////** utils-----------------------------------------------------------------*/
-//fun <ITEM:Any> WList<ITEM>.filter(block:(Wrap<ITEM>)->Boolean):Wrap<WList<ITEM>>
-//= foldRight(W{WList()}){ it, acc->if(block(it)) W{Cons(it, acc)} else acc}
-//tailrec fun <ITEM:Any> WList<ITEM>.sliceFrom(item:ITEM): WList<ITEM> = if(this is Cons && head != item) tail.sliceFrom(item) else this
-//tailrec fun <ITEM:Any> WList<ITEM>.slice(from:Int): WList<ITEM> = if(this is Cons && from > 0) tail.slice(from - 1) else this
-//inline fun <ITEM:Any> WList<ITEM>.slice(from:Int, to:Int): WList<ITEM> = slice(from).dropLast(to - from)
-//tailrec fun <ITEM:Any> WList<ITEM>.startsWith(target:WList<ITEM>):Boolean = when(this) {
-//    is Cons ->{
-//        when(target){
-//            is Cons -> if(head == target.head) when(target.tail){
-//                is Cons-> tail.startsWith(target.tail)
-//                is Nil -> true
-//            } else false
-//            is Nil -> false
-//        }
-//    }
-//    is Nil -> target is Nil
-//}
-//tailrec operator fun <ITEM:Any> WList<ITEM>.contains(target:ITEM):Boolean = when(this) {
-//    is Cons -> if(head == target) true else target in tail
-//    is Nil -> false
-//}
-//tailrec operator fun <ITEM:Any> WList<ITEM>.contains(target: WList<ITEM>):Boolean = when(this) {
-//    is Cons -> when (target) {
-//        is Cons -> if(startsWith(target)) true else target in tail
-//        is Nil -> false
-//    }
-//    is Nil -> target is Nil
-//}
+fun <ITEM:Any> WList<ITEM>.filter(block:(ITEM)->Boolean):WList<ITEM>
+= foldRight(WList()){ it, acc->if(block(it)) Cons(it, W{acc}) else acc}
+tailrec fun <ITEM:Any> WList<ITEM>.sliceFrom(item:ITEM): WList<ITEM>
+= if(this is Cons && head != item) tail.getOrFailEffect{return WList()}.sliceFrom(item) else this
+tailrec fun <ITEM:Any> WList<ITEM>.slice(from:Int): WList<ITEM>
+= if(this is Cons && from > 0) tail.getOrFailEffect{return WList()}.slice(from - 1) else this
+inline fun <ITEM:Any> WList<ITEM>.slice(from:Int, to:Int): WList<ITEM> = slice(from).dropLast(to - from)
+tailrec fun <ITEM:Any> WList<ITEM>.startsWith(target:WList<ITEM>):Boolean {
+    return when(this) {
+        is Cons ->{
+            when(target){
+                is Cons -> if(head == target.head) tail.getOrFailEffect{return false}.startsWith(target.tail.getOrFailEffect{return false}) else false
+                is Nil -> false
+            }
+        }
+        is Nil -> target is Nil
+    }
+}
+tailrec operator fun <ITEM:Any> WList<ITEM>.contains(target:ITEM):Boolean = when(this) {
+    is Cons -> if(head == target) true else target in tail.getOrFailEffect{return false}
+    is Nil -> false
+}
+tailrec operator fun <ITEM:Any> WList<ITEM>.contains(target: WList<ITEM>):Boolean = when(this) {
+    is Cons -> when (target) {
+        is Cons -> if(startsWith(target)) true else target in tail.getOrFailEffect{return false}
+        is Nil -> false
+    }
+    is Nil -> target is Nil
+}
 //inline fun <ITEM:Any, OTHER:Any, RESULT:Any> WList<ITEM>.zipWith(other: WList<OTHER>, noinline block:(ITEM, OTHER)->RESULT): WList<RESULT> {
 //    if(this is Nil || other is Nil) return WList()
 //    val thisSize = size
