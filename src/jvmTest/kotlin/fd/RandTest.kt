0e8c23d26c32dec6700a1140b4191b3a7cfdb89f
hika <hika00@gmail.com>
2023-10-14 15:24:55 +0900

    .

--- src/jvmTest/kotlin/fd/RandTest.kt
+++ src/jvmTest/kotlin/fd/RandTest.kt
@@ -9,7 +9,7 @@ class RandTest {
     @Test
     fun test1(){
         val rand1 = FRand.IntRand(42)
-        val (r1, rand2) = rand1.next()
+        val (r1, rand2) = rand1.nextInt()
         assertEquals(r1, 16159453)
 
         println(rand1.nextList(3))
