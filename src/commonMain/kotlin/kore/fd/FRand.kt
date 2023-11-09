6aa0c937fb5bbe499117f7aed523fd3270b3e3d7
hika <hika00@gmail.com>
2023-11-09 18:04:16 +0900

    .

--- src/commonMain/kotlin/kore/fd/FRand.kt
+++ src/commonMain/kotlin/kore/fd/FRand.kt
@@ -4,50 +4,42 @@ package kore.fd
 
 import kotlin.math.absoluteValue
 
-interface FRand{
-    data class IntRand internal constructor(val seed:Long):FRand{
-        override fun nextInt(): Pair<Int, FRand> {
-            val newSeed = (seed * 0x5DEECE66DL + 0xBL) and 0xFFFFFFFFFFFFL
-            return (newSeed ushr 16).toInt() to IntRand(newSeed)
-        }
-    }
-
+fun interface FIntState:(FIntState)->Pair<Int, FIntState>{
+    operator fun invoke():Pair<Int, FIntState> = invoke(this)
+}
+fun interface FGen<VALUE:Any>:(FIntState)->Pair<VALUE, FIntState> {
     companion object{
-        fun intRand(seed: Long):FRand = IntRand(seed)
+         operator fun <VALUE:Any> invoke(block:(FIntState)->Pair<VALUE, FIntState>):FGen<VALUE> = FGen{block(it)}
     }
-    fun nextInt():Pair<Int, FRand>
 }
-fun FRand.nextNonNegative():Pair<Int, FRand>{
-    val (r, next) = nextInt()
-    return (if(r == Int.MIN_VALUE) r + 1 else r).absoluteValue to next
+data class IntRand(val seed:Long):FIntState{
+    override fun invoke(state: FIntState): Pair<Int, FIntState> {
+        val newSeed = ((state as IntRand).seed * 0x5DEECE66DL + 0xBL) and 0xFFFFFFFFFFFFL
+        return (newSeed ushr 16).toInt() to IntRand(newSeed)
+    }
 }
-fun FRand.nextDouble():Pair<Double, FRand>{
-    val (r, next) = nextNonNegative()
-    return (if(r == 0) 0.0 else r.toDouble() / Int.MAX_VALUE.toDouble()) to next
+fun FIntState.nonNegative():Pair<Int, FIntState>
+= invoke().let{(v, state)->
+    (if(v == Int.MIN_VALUE) v + 1 else v).absoluteValue to state
 }
-fun FRand.nextIntDouble():Pair<Pair<Int, Double>, FRand>{
-    val (r1, next1) = nextInt()
-    val (r2, next2) = nextDouble()
+fun FIntState.double():Pair<Double, FIntState>
+= nonNegative().let{ (v, state)->
+    (if(v == 0) 0.0 else v.toDouble() / Int.MAX_VALUE.toDouble()) to state
+}
+fun FIntState.nextIntDouble():Pair<Pair<Int, Double>, FIntState>{
+    val (r1, next1) = invoke()
+    val (r2, next2) = next1.double()
     return r1 to r2 to next2
 }
-tailrec fun FRand._nextIntList(list:FList<Int>, count:Int):Pair<FList<Int>, FRand>
+private tailrec fun FIntState._intList(list:FList<Int>, count:Int):Pair<FList<Int>, FIntState>
 = if(count > 0){
-    val (r, rand) = nextInt()
-    rand._nextIntList(list + FList(r), count - 1)
+    val (r, state) = this()
+    state._intList(FList.Cons(r, list), count - 1)
 }else list to this
-fun FRand.nextIntList(count:Int):Pair<FList<Int>, FRand> = _nextIntList(FList(), count)
-
-
-fun interface FRandState<VALUE:Any> : (FRand)->Pair<VALUE, FRand> {
-    companion object{
-        operator fun <VALUE:Any> invoke(f:(FRand)->Pair<VALUE, FRand>):FRandState<VALUE> = FRandState{f(it)}
-    }
-}
-val a:FRandState<Int> = FRandState(FRand::nextNonNegative)
-
-fun <VALUE:Any> VALUE.toState(): FRandState<VALUE> = FRandState{this to it}
-fun <VALUE:Number, OTHER:Number> FRandState<VALUE>.map(block:(VALUE)->OTHER):FRandState<OTHER> = FRandState{
-    val (value, state) =this(it)
+fun FIntState.intList(count:Int):Pair<FList<Int>, FIntState> = _intList(FList(), count)
+fun <VALUE:Any> VALUE.toState(): FGen<VALUE> = FGen{this to it}
+fun <VALUE:Number, OTHER:Number> FGen<VALUE>.map(block:(VALUE)->OTHER):FGen<OTHER> = FGen{
+    val (value, state) = this(it)
     block(value) to state
 }
-fun FRandState<Int>.nonNegativeEven():FRandState<Int> = FRandState(FRand::nextNonNegative).map { it - (it % 2) }
+fun FGen<Int>.nonNegativeEven():FGen<Int> = FGen(FIntState::nonNegative).map { it - (it % 2) }
